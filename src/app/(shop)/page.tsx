5a0b4bcfd21d0b267f752dfe3ddb156cc63e5e42
Emanuel Lopez <emanuel-lopez13@hotmail.com>
2024-04-28 16:54:20 -0300

    Added ProductsGrid and CardProduct components

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,6 +2,10 @@ import Hero from "@/components/home/hero/Hero";
 import PromoSection from "@/components/home/promoSection/PromoSection";
 import Container from "@/components/ui/container/Container";
 import Title from "@/components/ui/title/Title";
+import ProductsGrid from "@/components/products/productsGrid/ProductsGrid";
+import { initialData } from "@/seed/seed";
+
+const products = initialData.products.slice(0, 15);
 
 export default function Home() {
   return (
@@ -9,7 +13,11 @@ export default function Home() {
       <Hero />
       <Container>
         <PromoSection />
-        <Title title="Nuestros productos" subtitle="Nuestros productos" />
+        <Title
+          title="Nuestros productos"
+          subtitle="Alguno de nuestros productos"
+        />
+        <ProductsGrid products={products} />
       </Container>
     </>
   );
